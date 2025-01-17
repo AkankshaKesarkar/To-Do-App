d33ea79b9cf6b2cb906eed383d4b6e71c89ef15c
AkankshaKesarkar <akankshakesarkar1361@gmail.com>
2025-01-17 22:44:18 +0530

    Updates

---
+++ precache-manifest.553ab826333668d415c6f6157b4e0cbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0547c174b78a3f16f08d5c1e3dba6c98",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "d90dcaa3c43836a21b0f",
+    "url": "/To-Do-List/static/css/main.b813eda7.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/To-Do-List/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "8e3a127b3eb410b6d841",
+    "url": "/To-Do-List/static/js/3.445915d0.chunk.js"
+  },
+  {
+    "revision": "d90dcaa3c43836a21b0f",
+    "url": "/To-Do-List/static/js/main.1adadd64.chunk.js"
+  },
+  {
+    "revision": "44df33b93d6a4bd83a9f",
+    "url": "/To-Do-List/static/js/runtime~main.9b22e6f0.js"
+  }
+]);
